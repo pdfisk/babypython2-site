c8ca1d7d9a79d218050dee8c811a1a9ed2cbf8cf
peter fisk <peter.fisk@gmail.com>
2022-11-20 12:32:57 -0500

    deploy

--- worker.js
+++ worker.js
@@ -1,9 +1,37 @@
-var i = 0;
+importScripts("https://cdn.jsdelivr.net/pyodide/v0.21.3/full/pyodide.js");
 
-function timedCount() {
-    i = i + 1;
-    postMessage(i);
-    setTimeout("timedCount()", 500);
+async function loadPyodideAndPackages() {
+    self.pyodide = await loadPyodide();
 }
 
-timedCount();
+let pyodideReadyPromise = loadPyodideAndPackages();
+
+self.onmessage = async (event) => {
+    await pyodideReadyPromise;
+    const id = event.data.id || 0;
+    const python = event.data.python || 'None';
+    const context = event.data.context || {};
+    for (const key of Object.keys(context)) {
+        self[key] = context[key];
+    }
+    try {
+        await self.pyodide.loadPackagesFromImports(python);
+        let results = await self.pyodide.runPythonAsync(python, context);
+        self.postMessage({
+            status: 'success',
+            id: id,
+            results: results
+        });
+    } catch (error) {
+        self.postMessage({
+            status: 'error',
+            id: id,
+            message: error.message
+        });
+    }
+};
+
+(async () => {
+    await pyodideReadyPromise;
+    self.postMessage({ status: 'ready' });
+})();

7db7c92172d604c118a70e17ed3d605aee77a60f
peter fisk <peter.fisk@gmail.com>
2022-11-20 14:52:16 -0500

    deploy

--- worker.js
+++ worker.js
@@ -1,39 +1,46 @@
-self.postMessage({status: 'progress', message: 'starting worker...'});
-importScripts("https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.js");
-self.postMessage({status: 'progress', message: 'imported scripts...'});
+try {
+    importScripts("https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.js");
 
-// async function loadPyodideAndPackages() {
-//     self.pyodide = await loadPyodide();
-// }
+    async function loadPyodideAndPackages() {
+        self.pyodide = await loadPyodide();
+    }
 
-// let pyodideReadyPromise = loadPyodideAndPackages();
+    let pyodideReadyPromise = loadPyodideAndPackages();
 
-// self.onmessage = async (event) => {
-//     await pyodideReadyPromise;
-//     const id = event.data.id || 0;
-//     const python = event.data.python || 'None';
-//     const context = event.data.context || {};
-//     for (const key of Object.keys(context)) {
-//         self[key] = context[key];
-//     }
-//     try {
-//         await self.pyodide.loadPackagesFromImports(python);
-//         let results = await self.pyodide.runPythonAsync(python, context);
-//         self.postMessage({
-//             status: 'success',
-//             id: id,
-//             results: results
-//         });
-//     } catch (error) {
-//         self.postMessage({
-//             status: 'error',
-//             id: id,
-//             message: error.message
-//         });
-//     }
-// };
+    self.onmessage = async (event) => {
+        await pyodideReadyPromise;
+        const id = event.data.id || 0;
+        const python = event.data.python || 'None';
+        const context = event.data.context || {};
+        for (const key of Object.keys(context)) {
+            self[key] = context[key];
+        }
+        try {
+            await self.pyodide.loadPackagesFromImports(python);
+            let results = await self.pyodide.runPythonAsync(python, context);
+            self.postMessage({
+                status: 'success',
+                id: id,
+                results: results
+            });
+        } catch (error) {
+            self.postMessage({
+                status: 'error',
+                id: id,
+                message: error.message
+            });
+        }
+    };
 
-// (async () => {
-//     await pyodideReadyPromise;
-//     self.postMessage({ status: 'ready' });
-// })();
+    (async () => {
+        await pyodideReadyPromise;
+        self.postMessage({ status: 'ready' });
+    })();
+}
+catch (error) {
+    self.postMessage({
+        status: 'error',
+        id: -1,
+        message: 'python load error: ' + error.message
+    });
+}
